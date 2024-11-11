803451ecc80aa4a02f49b297a627bd08199a730e
Faye <faye@kosli.com>
2024-11-11 17:45:22 +0100

    Upgrade base image

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -6,7 +6,7 @@ cd ${CYBER_DOJO_SANDBOX}
 CLASSES=.:`ls /mockito/*.jar | tr '\n' ':'`
 
 if javac --enable-preview --release 21 -Xlint:preview -Xlint:unchecked -Xlint:deprecation -cp $CLASSES *.java; then
-  java --enable-preview -jar /mockito/junit-platform-console-standalone-1.10.1.jar \
+  java --enable-preview -jar /mockito/junit-platform-console-standalone-1.11.3.jar \
       --disable-banner \
       --disable-ansi-colors \
       --details=tree \
