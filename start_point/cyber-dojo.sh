206ffa3d9dd8c63abdee20ae2d95d804e6dd003d
Faye <faye@kosli.com>
2024-11-13 17:31:56 +0100

    Add -javaagent and execute to java command in cyber-dojo.sh to remove warnings

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -6,8 +6,9 @@ cd ${CYBER_DOJO_SANDBOX}
 CLASSES=.:`ls /mockito/*.jar | tr '\n' ':'`
 
 if javac --enable-preview --release 21 -Xlint:preview -Xlint:unchecked -Xlint:deprecation -cp $CLASSES *.java; then
-  java --enable-preview -jar /mockito/junit-platform-console-standalone-1.11.3.jar \
-      --disable-banner \
+  java --enable-preview -javaagent:/mockito/byte-buddy-agent-1.15.4.jar \
+      -jar /mockito/junit-platform-console-standalone-1.11.3.jar \
+      execute --disable-banner \
       --disable-ansi-colors \
       --details=tree \
       --details-theme=ascii \
